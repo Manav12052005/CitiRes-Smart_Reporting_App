a1f24ad8fe3da76eb6b1f38b8586fc8d3a64556e
Yuan Shi <u7787385@anu.edu.au>
2024-09-18 19:12:06 +1000

    add some sample reports.

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         arrayList = new ArrayList<>();
-        arrayList.add(new Report("Today", "The road is empty", "on campus"));
-        arrayList.add(new Report("Yesterday", "The water is bad", "on campus"));
+        arrayList.add(new Report("Traffic Lights Malfunctioning at Major Intersection", "They remain stuck on red, causing significant traffic congestion ", "Main st"));
+        arrayList.add(new Report("Streetlight Outage", "Several streetlights have been out for over two weeks, causing a lot of trouble.", "on campus"));
+        arrayList.add(new Report("Overgrown Vegetation Obstructing Sidewalk", "It is obstructing the pathway and making it difficult for pedestrians, especially those with mobility issues, to pass through.", "East Park"));
 
         ListView listView = findViewById(R.id.reports_list);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
