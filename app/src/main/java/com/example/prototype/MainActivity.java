c575bb4653d235b85a956a757fd426854d9809a0
Yuan Shi <u7787385@anu.edu.au>
2024-09-18 19:05:49 +1000

    Add a simple dashboard user interface to the project.

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.prototype;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Report> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        arrayList = new ArrayList<>();
+        arrayList.add(new Report("Today", "The road is empty", "on campus"));
+        arrayList.add(new Report("Yesterday", "The water is bad", "on campus"));
+
+        ListView listView = findViewById(R.id.reports_list);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/prototype/Report.java
@@ -0,0 +1,46 @@
+package com.example.prototype;
+
+
+public class Report {
+    private String title;
+    private String description;
+    private String location;
+
+
+    public Report(String title, String description, String location) {
+        this.title = title;
+        this.description = description;
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(title.toUpperCase()).append("\n").append(description).append("\n").append(location);
+        return sb.toString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
