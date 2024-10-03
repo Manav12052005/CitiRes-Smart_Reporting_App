fbbeffbd41a2b1729a48b3545b4380b6cfd60dfb
Yuan Shi <u7787385@anu.edu.au>
2024-10-04 00:53:37 +1000

    Finish DataStream feature. Switch between light/night theme based on time.

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,32 +19,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     AVLTree<Report> avlTree = new AVLTree<>();
     ReportAdapter adapter;
+    ListView listView;
     ImageButton menuDashboard;
     ImageButton menuSearch;
+    Button addReportButton;
+    List<Report> loadedReports;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        applyTheme();
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.reports_list);
+        listView = findViewById(R.id.reports_list);
 
-        List<Report> reports = loadData("reports_dataset.json");
-        for (Report report : reports) {
+        loadedReports = loadData("reports.json");
+
+        for (Report report : loadedReports) {
             avlTree.put(report.getReportId(), report);
         }
-        //
-        List<Report> reportList = new ArrayList<>(avlTree.fromLargeToSmall());
-        adapter = new ReportAdapter(this, reportList);
+
+        adapter = new ReportAdapter(this, avlTree);
         listView.setAdapter(adapter);
 
-        Button addReportButton = findViewById(R.id.add_report_button);
+        addReportButton = findViewById(R.id.add_report_button);
         addReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,20 +59,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        menuDashboard = findViewById(R.id.menu_dashboard);
 
         menuDashboard = (ImageButton) findViewById(R.id.menu_dashboard);
-        menuDashboard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
+
+        menuSearch = (ImageButton) findViewById(R.id.menu_search);
+
+        title = findViewById(R.id.dashboard_title);
+    }
+
+    Thread streamThread;
+    private static final LocalTime DAY_START = LocalTime.of(6, 0);
+    private static final LocalTime NIGHT_START = LocalTime.of(18, 0);
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startStreamThread();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopStreamThread();
+    }
+
+    private void stopStreamThread() {
+        super.onStop();
+        streamThread.interrupt();
+    }
+
+    private void startStreamThread() {
+        streamThread = new Thread(() -> {
+            while (true) {
+                applyTheme();
+                try {
+                    Thread.sleep(1000 * 60);
+                } catch (InterruptedException e) {
+
+                }
             }
         });
-        menuSearch = (ImageButton) findViewById(R.id.menu_search);
+        streamThread.start();
+    }
+
+    private void applyTheme() {
+        LocalTime currentTime = LocalDateTime.now().toLocalTime();
+
+        if (currentTime.isAfter(DAY_START) && currentTime.isBefore(NIGHT_START)) {
+            runOnUiThread(() -> {
+                setTheme(R.style.AppTheme_Day);
+            });
+        } else {
+            runOnUiThread(() -> {
+                setTheme(R.style.AppTheme_Night);
+            });
+        }
     }
 
     public List<Report> loadData(String fileName) {
-
         GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(Report.class, new ReportAdapterJson());
         Gson gson = gsonBuilder.create();
 
