7b40f9bf82aca2fc001550e5fa8a46249604dcb4
Amogh Agarwal <u7782814@anu.edu.au>
2024-10-05 13:29:44 +1000

    reportWork

--- app/src/main/java/com/example/prototype/ReportActivity.java
+++ app/src/main/java/com/example/prototype/ReportActivity.java
@@ -1,33 +1,24 @@
 package com.example.prototype;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-public class ReportActivity extends Activity {
-    private Spinner spinnerLocation, spinnerCategory, spinnerPriority;
-    private EditText editTextDescription, editTextTaskId;
-    private Button buttonAddPicture, buttonSubmit;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report);
-
-        // Initialize UI elements
-        spinnerLocation = findViewById(R.id.spinner_location);
-        spinnerCategory = findViewById(R.id.spinner_category);
-        spinnerPriority = findViewById(R.id.spinner_priority);
-        editTextDescription = findViewById(R.id.edittext_description);
-        editTextTaskId = findViewById(R.id.edittext_task_id);
-        buttonAddPicture = findViewById(R.id.button_add_picture);
-        buttonSubmit = findViewById(R.id.button_submit);
-
-        // TODO: Set up Spinners with adapters and button actions
-
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_report2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
