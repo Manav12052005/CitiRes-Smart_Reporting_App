43bd6f9e1b0b46813e3c78b061f42273f2f6cebd
u7781798 <u7781798@anu.edu.au>
2024-10-05 17:03:46 +1000

    last step before connecting files

---
+++ app/src/main/java/com/example/prototype/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.example.prototype;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class HomeFragment extends Fragment {
+
+
+    String API_KEY ="implementation 'com.github.bumptech.glide:glide:4.16.0" ;
+    RecyclerView recyclerView;
+    Adapter adapter;
+    ArrayList<Model> modelArrayList;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View v = inflater.inflate(R.layout.fragment_home,null);
+        recyclerView = v.findViewById(R.id.home_recylerview);
+        modelArrayList = new ArrayList<>();
+        adapter = new Adapter(getContext(),modelArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        getNews();
+        return v;
+    }
+
+
+    void getNews(){
+        ApiUtilities.getApiInterface().getNews("us",100,API_KEY).enqueue(new Callback<MainNews>() {
+            @Override
+            public void onResponse(Call<MainNews> call, Response<MainNews> response) {
+                    if(response.isSuccessful()){
+                        modelArrayList.addAll(response.body().getArticles());
+                        adapter.notifyDataSetChanged();
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<MainNews> call, Throwable t) {
+
+            }
+        });
+    }
+}
